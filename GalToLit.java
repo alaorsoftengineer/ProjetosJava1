54ba63c912af8715a7366488a22e873e029b238b
Alaor Castelano <alaor.softengineer@gmail.com>
2021-06-27 19:18:53 -0300

    Add files via upload
    Measures converter: convert gallons to liters

---
+++ GalToLit.java
@@ -0,0 +1,23 @@
+
+// Este programa converte Galões para Litros.
+
+package galtolit;
+
+
+public class GalToLit {
+
+    
+    public static void main(String[] args) {
+        
+        double gallons, liters; // contem núm. de laões e litros
+        
+        gallons = 10; // começa com 10 galões
+        
+        liters = gallons * 3.7854; // converte para litros
+        
+        System.out.println(gallons + " " + "gallons is" + " " + liters + " " + "liters.");
+        
+          
+    }
+    
+}
