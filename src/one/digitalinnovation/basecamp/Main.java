623109d9f6035182e9313e03cace9530505dc64d
Alaor Castelano <alaor.softengineer@gmail.com>
2022-01-17 17:31:42 -0300

    Exercicio Sobrecarga

---
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // Quadrilatero
+        System.out.println("Exercicio Quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7, 8, 9);
+
+    }
+}
