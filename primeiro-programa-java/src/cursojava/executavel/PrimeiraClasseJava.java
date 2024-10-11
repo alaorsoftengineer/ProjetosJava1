49a391d9bac2c15bb9d56453ae5c8f02f8ceb593
Alaor Castelano <alaor.softengineer@gmail.com>
2024-10-11 18:23:13 -0300

    Criação do Projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,21 @@
+package cursojava.executavel;
+
+import java.util.Scanner;
+
+public class PrimeiraClasseJava {
+	
+	
+	/*Main é um método autoexecutavel em Java*/
+	public static void main(String[] args) {
+		
+		
+	    /*Escreve texto no Console*/
+		System.out.println("Meu primeiro programa em Java");
+		
+		
+		
+		
+		
+	}
+
+}
