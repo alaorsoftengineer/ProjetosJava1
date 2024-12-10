c459b371f4c33bdfe3bb6bf83cfff08060712be5
Alaor Castelano <alaor.softengineer@gmail.com>
2024-12-10 08:18:02 -0300

    Adicionado no Aluno1, as Disciplinas.

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,20 +1,88 @@
 package cursojava.executavel;
 
-import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+import cursojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
+	
+	
 	
 	
 	/*Main é um método autoexecutavel em Java*/
 	public static void main(String[] args) {
 		
 		
-	    /*Escreve texto no Console*/
-		System.out.println("Meu primeiro programa em Java");
 		
-		/*uma nova linha de comentario*/
-		System.out.println("Linguagem orientada a objetos");
+		/* new Aluno() é uma instancia (Criação de um objeto) */
+		
+		/* aluno1 (variável) é uma referência para o objeto Aluno() */
+		
+		
+		String nome = JOptionPane.showInputDialog("Qual o nome do aluno ?");
+		String idade = JOptionPane.showInputDialog("Qual a idade ?");
+		
+		String disciplina1 = JOptionPane.showInputDialog("Qual a Disciplina 1 ?");
+		String Nota1 = JOptionPane.showInputDialog("Qual a Nota 1 ?");
+		
+		String disciplina2 = JOptionPane.showInputDialog("Qual a Disciplina 2 ?");
+		String Nota2 = JOptionPane.showInputDialog("Qual a Nota 2 ?");
+		
+		String disciplina3 = JOptionPane.showInputDialog("Qual a Disciplina 3 ?");
+		String Nota3 = JOptionPane.showInputDialog("Qual a Nota 3 ?");
+		
+		String disciplina4 = JOptionPane.showInputDialog("Qual a Disciplina 4 ?");
+		String Nota4 = JOptionPane.showInputDialog("Qual a Nota 4 ?");
+		
+		
+		
+		Aluno aluno1 = new Aluno(); /* Aqui será o João */
+		
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade));
+		aluno1.setDataNascimento("14/10/87");
+		aluno1.setRegistroGeral("345673445");
+		aluno1.setNumeroCpf("026.456.786-20");
+		aluno1.setNomeMae("Joana");
+		aluno1.setNomePai("Pedro");
+		aluno1.setDataMatricula("28/12/2020");
+		aluno1.setNomeEscola("Atlas");
+		aluno1.setSerieMatriculado("Setimo");
+		aluno1.setDisciplina1(disciplina1);
+		aluno1.setDisciplina2(disciplina2);
+		aluno1.setDisciplina3(disciplina3);
+		aluno1.setDisciplina4(disciplina4);
+		aluno1.setNota1(90);
+		aluno1.setNota2(80.8);
+		aluno1.setNota3(70.9);
+		aluno1.setNota4(90);
+		
+		
+		
+		System.out.println("Nome do aluno1 é = " + aluno1.getNome());
+		System.out.println("Idade do aluno1 é = " + aluno1.getIdade());
+		System.out.println("O CPF do aluno1 é = " + aluno1.getNumeroCpf());
+		System.out.println("A média da Nota do aluno1 é = " + aluno1.getMediaNota());
+		System.out.println("Resultado é: " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		
+		
+		
+		/* ========================================================================== */
+		
+		
+		
+		Aluno aluno2 = new Aluno(); /* Aqui será o Pedro */
+		
+		Aluno aluno3 = new Aluno(); /* Aqui será o Alex */
+		
+		Aluno aluno4 = new Aluno("Maria");
+		
+		Aluno aluno5 = new Aluno("José", 50);
+		
+		
+		
 		
 	}
-
+	
+	
 }
