155699bb78f97cffbb943807c90a817bb54370d9
Alaor Castelano <alaor.softengineer@gmail.com>
2025-08-30 15:55:03 -0300

    Novas formas de atribuir valores em Arrays.

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -1,32 +1,24 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 public class ArrayVetor {
 	
 	/* Usado para executar codigo */
 	public static void main(String[] args) {
 		
 		
-		/* Array pode ser de todos tipos de dados e objetos também */
 		
-		
-		/* Array sempre deve ter a quantidade de posiçoes definidas */
-		double[] notas = new double[4];
-		
-		
-		/* Atribuir valor nas posiçoes do array */
-		
-		notas[0] = 7.8;
-		notas[1] = 8.7;
-		notas[2] = 9.7;
-		notas[3] = 9.9;
+		String[] valores = {"alex", "90", "Curso de java", "contato@jdevtreinamento.com.br"};
 		
 		
 		
-		for (int pos = 0; pos < 4; pos++) {
-			System.out.println("Nota" + " " + (pos + 1) + " " + "é =" + " " + notas[pos]);
+		
+		for (int pos = 0; pos < valores.length; pos++) {
+			
+			System.out.println("Valor na posição " + pos + " é igual = " + valores[pos]);
 			
 		}
-		
 		
 	}
 
