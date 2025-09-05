368b4478b0003bb84e0686e50916ea09947e2c7b
Alaor Castelano <alaor.softengineer@gmail.com>
2025-09-05 20:05:17 -0300

    Fila / Pilha

--- primeiro-programa-java/src/cursojava/thread/ObjetoFilaThread.java
+++ primeiro-programa-java/src/cursojava/thread/ObjetoFilaThread.java
@@ -1,5 +1,7 @@
 package cursojava.thread;
 
+import java.util.Objects;
+
 public class ObjetoFilaThread {
 	
 	
@@ -22,6 +24,29 @@ public class ObjetoFilaThread {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	
+	
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, nome);
+	}
+	
+	
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ObjetoFilaThread other = (ObjetoFilaThread) obj;
+		return Objects.equals(email, other.email) && Objects.equals(nome, other.nome);
 	}
 	
 	
