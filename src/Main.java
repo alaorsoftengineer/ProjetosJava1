345e57f2588337215dbf34f380fdbd56e9f3d308
Alaor Castelano <alaor.softengineer@gmail.com>
2026-03-07 17:55:04 -0300

    New Project in Java

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+       String name = "Alaor Castelano";
+       String food = "pizza";
+
+
+        System.out.println("My favorite food is " + " " + food);
+
+
+
+    }
+}
