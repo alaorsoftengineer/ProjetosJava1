0c2f1fac19b92777316269b12f937745982a4eba
Alaor Castelano <alaor.softengineer@gmail.com>
2026-03-09 18:34:50 -0300

    Scanners in Java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-       String name = "Alaor Castelano";
-       String food = "pizza";
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
 
-        System.out.println("My favorite food is " + " " + food);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
 
+        System.out.println("What is your gpa: ");
+        double gpa = scanner.nextDouble();
+
+        System.out.println("Hello, " + name);
+        System.out.println("You are" + " " + age + " years old.");
+        System.out.println("You gpa is:" + " " + gpa);
+
+        scanner.close();
 
 
     }
 }
+
+
+
+
